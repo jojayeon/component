5e624d9a64c79d3498dadcf90a12d8961a9046b1
jojayeon <jojayeon6152@gmail.com>
2024-06-11 10:55:28 +0900

    a에 동적으로 키,값 추가

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
-const a = () => {
+const a = (name, age, job) => {
   return {
-    name: "",
-    age: 0,
-    job: "",
+    name: name,
+    age: age,
+    job: job,
   };
 };
 // console.log(a());
-
-const b = a(); 
-console.log(b);
+//위는 정적인데
+const b = a("조자연",26,"앤디 형"); 
+console.log("첫번째 b:",b);
+b.address = "대전";//이 부분에서 동적으로 넣는 것도 가능해서 유연하다.
+console.log("두번째 b:",b);
 
