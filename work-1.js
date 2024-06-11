f293e9ff1dd31c054743c4119cb64d1d4aa41a6e
jojayeon <jojayeon6152@gmail.com>
2024-06-11 11:03:07 +0900

    함수 만들기, 호출

--- work-1.js
+++ work-1.js
@@ -1,1 +1,21 @@
-console.log("work1");
+function formatHTML() {
+  let htmlData = {
+    title:"조자연",
+    h1 : "난",
+    p : "앤디 형임",
+  }
+  let html = `
+  <html>
+  <head>
+    <tltie>${htmlData.title}</tltie>
+  </head>
+    <body>
+      <h1>${htmlData.h1}<h1>
+      <p>${htmlData.p}<p>
+    </body>
+  </html>  
+  `;
+  return html;
+}
+
+console.log(formatHTML());
